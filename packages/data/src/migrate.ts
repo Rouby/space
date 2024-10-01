eac10993e3e910a0c2854d75c3e318e4400c282d
Jonathan Burke <jonathan.burke.1311@googlemail.com>
2024-10-02 01:12:02 +0200

    chore: add migrations build
    Signed-off-by: Jonathan Burke <jonathan.burke.1311@googlemail.com>

--- packages/data/src/migrate.ts
+++ packages/data/src/migrate.ts
@@ -1,10 +1,13 @@
 import { migrate } from "drizzle-orm/postgres-js/migrator";
+import { resolve } from "node:path";
 import { getConnection, getDrizzle } from "./index.ts";
 
 const connection = await getConnection();
 
 const db = getDrizzle(connection);
 
-await migrate(db, { migrationsFolder: "./drizzle" });
+await migrate(db, {
+	migrationsFolder: resolve(import.meta.filename, "../../drizzle"),
+});
 
 await connection.end();
